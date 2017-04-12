7822d619daa1ef68dc5183cae69b7d0bde76202c
Leigh Fisher <leighfisher@Jamess-MacBook-Air-2.local>
2017-04-11 22:32:46 -0500

    the force

--- Flash.js
+++ Flash.js
@@ -3,7 +3,7 @@
 // inquirer npm package
 var inquirer = require("inquirer");
 console.log("This is not the app you are looking for");
-
+var userAnswer = process.argv[2]
 // card constructor 
 function basicCard(front,back) {
   this.front = front;
@@ -14,29 +14,8 @@ function clozeCard(text,cloze) {
   this.cloze = cloze
 };
 // quiz method 
-grandMasterFlash.prototype.quiz = function() {
-  console.log("Name: " + this.batman + "\nPosition: " + this.superman + "\nAge: " +
-  this.age + "\nLanguages: " + this.language + "\nLanguages: " + this.language +  "\nLanguages: " + this.language);
-};
+ var question1 = new basicCard("what is the meaning of life?", "42");
 
-// runs inquirer and asks the user a series of questions whose replies are
-// stored within the variable answers inside of the .then statement.
-inquirer.prompt([
-  {
-    name: "name",
-    message: "What is your name?"
-  }, {
-    name: "position",
-    message: "What is your current position?"
-  }, {
-    name: "age",
-    message: "How old are you?"
-  }, {
-    name: "language",
-    message: "What is your favorite programming language?"
-  }
-]).then(function(answers) {
+ var question2 = new clozeCard("May the . . . be with you", "force");
 
-  var question = new basicCard(answers.name, answers.position, answers.age, answers.language);
- question.printInfo();
-});
+ 
